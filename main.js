8962ff6fed83e5544a89741215537077a06041a6
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-06-17 15:32:40 +0200

    Fixed

--- main.js
+++ main.js
@@ -5,13 +5,26 @@ bot.on("ready", () => {
     console.log("GuessTheNumber is Ready!");
 });
 
-let number = 1337; // You can change it through /number command
 let limit = 2000; // You can change it through /limit command
+let number = Math.floor(Math.random()* Math.floor(limit)); // You can custom it through /number command and reroll it through /reroll
 let ownerID = 'put your own user ID here';
 let channelID = 'put the channel ID where you want the game';
 let token = `put your bot's token here`
 
 bot.on('message', message => {
+    if(message.content == "/viewnumb") {
+        if(!message.author.id == ownerID) return message.reply(`You don't have the permission to run this command.`);
+        message.reply(`The current number is ${number}`);
+    }
+    if(message.content == "/viewlim") {
+        if(!message.author.id == ownerID) return message.reply(`You don't have the permission to run this command.`);
+        message.reply(`The current limit is ${limit}`);
+    }
+    if(message.content == "/reroll") {
+        if(!message.author.id == ownerID) return message.reply(`You don't have the permission to run this command.`);
+        number = Math.floor(Math.random()* Math.floor(limit));
+        message.author.send(`The new number is ${number}`);
+    }
     if(message.content.startsWith("/number")) {
         if(!message.author.id == ownerID) return message.reply(`You don't have the permission to run this command.`);
         const args = message.content.slice(1).trim().split(/ +/g);
@@ -31,24 +44,24 @@ bot.on('message', message => {
     if(message.author.bot) return;
     if(message.channel.id === channelID) {
         if(!message.content.isNaN) {
-            if(message.content > limit) return message.reply(`The number is between 1 and ${limit}! Try again`);
-            if(message.content < 1) return message.reply(`The number cannot be negative! Try again`);
+            if(message.content > limit) return message.reply(`The number is between 1 and ${limit}! Try again`).then(sent => sent.delete(10000));
+            if(message.content < 1) return message.reply(`The number cannot be negative! Try again`).then(sent => sent.delete(10000));
             if(message.content == number) {
-                var everyone =  message.guild.id;
+                var everyone =  message.guild.roles.find("name", "@everyone");
                 bot.channels.find("id", channelID).overwritePermissions(everyone, {
                     VIEW_CHANNEL: true,
                     SEND_MESSAGES: false
                 });
-                message.channel.send(`<@${message.author.id}> found the correct number! It was ${number}. The channel will be unlocked in 1 minute.`);
                 function unlock() {
                     bot.channels.find("id", channelID).overwritePermissions(everyone, {
                         VIEW_CHANNEL: true,
                         SEND_MESSAGES: true
                     });
                 }
+		message.channel.send(`<@${message.author.id}> found the correct number! It was ${number}. The channel will be unlocked in 1 minute.`);
                 setTimeout(unlock, 60000);
             }
-        } else return message.reply(`Your message is not a number.`);
+        } else return
     }
 });
 bot.login(token);
